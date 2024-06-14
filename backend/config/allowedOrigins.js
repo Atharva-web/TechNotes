c38dfe861fafaea44b294c118bcbfb6d2c4e458c
Atharva-web <ath3arv8a2@gmail.com>
2024-06-14 19:10:54 +0530

    further configurations, loggers and CORS access specification

---
+++ backend/config/allowedOrigins.js
@@ -0,0 +1,7 @@
+const allowedOrigins = [
+    "http://localhost:3000",
+    "https://example.com",
+    "https://www.youtube.com"
+];
+
+module.exports = allowedOrigins;
--- backend/server.js
+++ backend/server.js
@@ -1,9 +1,27 @@
 const express = require("express");
-const app = express();
 const path = require("path");
+const cookieParser = require("cookie-parser");
+const cors = require("cors");
+
+const { logger } = require("./middleware/logger");
+const errorHandler = require("./middleware/errorHandler");
+
+const corsOptions = require("./config/corsOptions");
+
+const app = express();
+
 const PORT = process.env.PORT || 3500;
 
-app.use("/", express.static(path.join(__dirname, "/public")));
+app.use(cookieParser());
+
+app.use(logger);
+
+// app.use(cors()); // no optios, accessible to all, is a public api
+app.use(cors(corsOptions));
+
+app.use(express.json());
+
+app.use("/", express.static(path.join(__dirname, "public")));
 app.use("/", require("./routes/root"));
 
 app.all("*", (req, res) => {
@@ -18,6 +36,8 @@ app.all("*", (req, res) => {
         res.type("txt").send("404 Not Found");
     }
 });
+
+app.use(errorHandler); // deliberatly writing at the bottom
 
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
